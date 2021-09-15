e8cdfa85f93ec7114382b334e236a748374554e6
vmagana3 <vmagana3@ucol.mx>
2021-09-15 09:56:32 -0500

    Angular calculator backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculadoraApp';
+
+  //Properties//
+
+  title = 'Calculadora';
+
+  primerNumero: number = 0;
+  segundoNumero: number = 0;
+  resultado: number = 0;
+
+  //Methods//
+  sumar(): void{
+    console.log("Primer Numero:" +  typeof(this.primerNumero));
+    console.log("Segundo Numero:" + typeof(this.segundoNumero));
+    console.log(parseInt(this.primerNumero) + parseInt(this.segundoNumero));
+
+  }
+
 }
